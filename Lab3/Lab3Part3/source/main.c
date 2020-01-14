4e332671f5281a72a3e99cf47f5370fb54ec0cba
Ted Kim <tkim094@wch136-33.cs.ucr.edu>
2020-01-14 00:06:09 -0800

    All test cases passed, change Part3Part3 to Lab3Part3

--- Lab3/Lab3Part3/source/main.c
+++ Lab3/Lab3Part3/source/main.c
@@ -9,7 +9,7 @@
 *	code, is my own original work.
 */
 #include <avr/io.h>
-#include “simAVRHeader.h”
+#include "simAVRHeader.h"
 
 unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
   return (b ? x | (0x01 << k) : x & ~(0x01 << k));
